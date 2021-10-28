9cbf7ad704e3859dfc91ec56ec1336ffcf391b11
Pearl Arora <67559310+pearlarora@users.noreply.github.com>
2021-10-28 14:46:44 +0530

    mentors added

---
+++ src/components/Mentors/Mentors.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Mentors.css";
+import right from "../../images/right.png";
+import left from "../../images/left.png";
+import mentor from "../../images/mentor.png";
+
+function Mentors() {
+  return (
+    <div className="mentor">
+      <div className="about__heading">
+        <img src={left} alt="" />
+        <h1>Mentors</h1>
+        <img src={right} alt="" />
+      </div>
+      <div className="mentor-cards">
+        <div className="mentor-card">
+          <img src={mentor} alt="mentor" />
+          <div className="mentor-details">
+            <h3>Mentor Name</h3>
+            <p>Software Developer at Zomato</p>
+          </div>
+        </div>
+        <div className="mentor-card">
+          <img src={mentor} alt="mentor" />
+          <div className="mentor-details">
+            <h3>Mentor Name</h3>
+            <p>Software Developer at Zomato</p>
+          </div>
+        </div>
+        <div className="mentor-card">
+          <img src={mentor} alt="mentor" />
+          <div className="mentor-details">
+            <h3>Mentor Name</h3>
+            <p>Software Developer at Zomato</p>
+          </div>
+        </div>
+        <div className="mentor-card">
+          <img src={mentor} alt="mentor" />
+          <div className="mentor-details">
+            <h3>Mentor Name</h3>
+            <p>Software Developer at Zomato</p>
+          </div>
+        </div>
+        <div className="mentor-card">
+          <img src={mentor} alt="mentor" />
+          <div className="mentor-details">
+            <h3>Mentor Name</h3>
+            <p>Software Developer at Zomato</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Mentors;
